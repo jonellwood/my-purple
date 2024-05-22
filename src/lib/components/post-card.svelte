b4a9fe9bf27831c979cf79a8bdf668739e2d9657
Jon Ellwood <jonellwood@gmail.com>
2024-05-22 14:03:13 -0400

    manage post buttons - no tabbing

--- src/lib/components/post-card.svelte
+++ src/lib/components/post-card.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 	import type { PostWithUser } from '$lib/server/schemas';
-
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { MoreVertical } from 'lucide-svelte';
+	import { Trash } from 'lucide-svelte';
+	import { SquarePen } from 'lucide-svelte';
+	import { buttonVariants } from './ui/button';
+	import { Description } from 'formsnap';
+	import Button from './ui/button/button.svelte';
 	type Props = {
 		post: PostWithUser;
 	};
 	let { post } = $props<Props>();
+
+	let deleteDialogOpen = $state(false);
+	let editDialogOpen = $state(false);
 </script>
 
 <Card.Root>
-	<Card.Header>
+	<Card.Header class="flex-row items-center justify-between">
 		<Card.Title>
 			{post.title}
 		</Card.Title>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger class={buttonVariants({ size: 'icon', variant: 'ghost' })}>
+				<MoreVertical class="size-4" />
+				<span class="sr-only"> Manage Posts </span>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				<DropdownMenu.Item>
+					<SquarePen class="size-4 mr-2" />
+					Edit
+				</DropdownMenu.Item>
+				<DropdownMenu.Item onclick={() => (deleteDialogOpen = true)}>
+					<Trash class="size-4 mr-2" />
+					Delete
+				</DropdownMenu.Item>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
 	</Card.Header>
 	<Card.Content>
 		{post.content}
@@ -21,3 +47,16 @@
 		By: {post.user.username}
 	</Card.Footer>
 </Card.Root>
+
+<AlertDialog.Root bind:open={deleteDialogOpen}>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Delete Post</AlertDialog.Title>
+			<AlertDialog.Description>Are you sure about this? ... Sinner!</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<Button variant="destructive">Yep</Button>
+			<Button variant="outline" onclick={() => (deleteDialogOpen = false)}>Nope</Button>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
